deebf33706f9582edee09ff230a80e86d2a773f0
Prit-das <pdas99083@gmail.com>
2024-09-06 20:57:15 +0400

    first initialization

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      xs: '220px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+    },
+  },
+  plugins: [],
+}
