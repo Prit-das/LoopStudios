f1fe6fdda303912f9c52944ba066199aa94756c7
Prit-das <pdas99083@gmail.com>
2024-09-12 07:09:03 +0400

    correction

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
+const menu = document.getElementById('mobile')
 
-btn.addEventListener('click', navToggle)
-
-function navToggle() {
-  btn.classList.toggle('open')
-  menu.classList.toggle('flex')
-  menu.classList.toggle('hidden')
-}
+btn.addEventListener('click', function () {
+  menu.classList.toggle('open');
+});
